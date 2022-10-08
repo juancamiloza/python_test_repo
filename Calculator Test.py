bb2fdecdc061f3d97332436fa5041d333571b0b6
JuanMac <juancamiloza@gmail.com>
2022-10-08 20:33:17 +0200

    Update on calc app to test Git settings on Mac

--- Calculator Test.py
+++ Calculator Test.py
@@ -6,12 +6,15 @@ def calculator():
         '*': operator.mul,
         '/': operator.truediv
     }
+    operations_list = ["+", "-", "*", "/"]
     print("Welcome to J's Calculator 2.0")
     first_number = float(input("Enter your first number: "))
     operation = input("What operation you want to calculate? (+,-,*,/)")
+    while operation not in operations_list:
+        operation = input("Wrong input, please chose an operation from (+,-,*,/)")
     second_number = float(input("Enter your second number: "))
     answer = (ops[operation](first_number, second_number))
     answer_two_decimal = "{:.2f}" .format(answer)
     print(f"Full answer: {answer}")
-    print(f"Aproximated answer: {answer_two_decimal}")
+    print(f"Approximated answer: {answer_two_decimal}")
 calculator()
