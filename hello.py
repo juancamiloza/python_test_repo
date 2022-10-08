e4708c2cdf97f5a270f3edb535357a138d57ea7e
JuanMac <juancamiloza@gmail.com>
2022-10-08 22:06:48 +0200

    New Hello app function and implementation in diceV2

---
+++ hello.py
@@ -0,0 +1,28 @@
+#-------------------------------------------------
+# import hello
+#
+# program_state = True           # Insert the heading in new app
+#                                # Replace "app" with your app's name
+# while program_state:
+#
+#     hello.program_name("app")
+#--------------------------------------------------
+
+import time
+
+
+def program_name(pname):
+    app_challenge = False
+    program_states = ["Q", "QUIT"]
+    print(f"Welcome to {pname}")
+    first_action = str.upper(input("Press ENTER to continue, write QUIT to exit "))
+    while not app_challenge:
+        if first_action in program_states:
+            print(f"Thank you for using {pname}")
+            time.sleep(5)
+            exit()
+        elif len(first_action) == 0:
+            app_challenge = True
+        else:
+            print("Wrong command")
+            first_action = str.upper(input("Press ENTER to continue, write QUIT to exit "))
