45ae3255949847ad616a38d4ec31b3f8c5645bd2
JuanMac <juancamiloza@gmail.com>
2022-10-09 06:37:27 +0200

    Fixed ValueError for invalind # of dice input

--- DiceProgramV2.py
+++ DiceProgramV2.py
@@ -8,25 +8,28 @@ while program_state:
 
     hello.program_name("Dice Roll V2")
 
-    dice_faces = int(input("How Many faces you would like your dices to have? "))
-    roll_state = True
-    while roll_state:
-        try:
-            dices = [random.randint(1, dice_faces)]
-            number_of_dices = int(input("How many dices you want roll? (0 to go back) "))
-            if number_of_dices == 1:
-                print("Outcome:")
-                print(dices)
-            elif number_of_dices == 0:
-                roll_state = False
-            else:
-                create = 1
-                # for create, number_of_dices in enumerate(dices.append(random.randint(1,dice_faces))):
-                while create != number_of_dices:
-                    dices.append(random.randint(1, dice_faces))
-                    create += 1
-                print("Outcome:")
-                print(*dices, sep="|")
-                print(f"For {len(dices)} dice")
-        except ValueError:
-            print("Wrong value, type a number")
+    try:
+        dice_faces = int(input("How Many faces you would like your dices to have? "))
+        roll_state = True
+        while roll_state:
+            try:
+                dices = [random.randint(1, dice_faces)]
+                number_of_dices = int(input("How many dices you want roll? (0 to go back) "))
+                if number_of_dices == 1:
+                    print("Outcome:")
+                    print(dices)
+                elif number_of_dices == 0:
+                    roll_state = False
+                else:
+                    create = 1
+                    # for create, number_of_dices in enumerate(dices.append(random.randint(1,dice_faces))):
+                    while create != number_of_dices:
+                        dices.append(random.randint(1, dice_faces))
+                        create += 1
+                    print("Outcome:")
+                    print(*dices, sep="|")
+                    print(f"For {len(dices)} dice")
+            except ValueError:
+                print("Wrong value, type a number")
+    except ValueError:
+        print("Wrong value, type a number")
