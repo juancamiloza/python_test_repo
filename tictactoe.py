b2a7866cd67c594741a58244bb6882136a3f1599
Juan Camilo Zapata <juancamiloza@gmail.com>
2022-10-11 02:29:43 +0200

    implemented random choice

--- tictactoe.py
+++ tictactoe.py
@@ -16,6 +16,7 @@ import copy
 size = 3
 help = [[" 1 "," 2 "," 3 "],[" 4 "," 5 "," 6 "],[" 7 "," 8 "," 9 "]]
 glist = []
+clist = []
 for c in range (size):
     glist.append(["   "])
 for d in range (size-1):
@@ -55,7 +56,38 @@ def print_help():
             break
     print(f"\nWhen promped, type the number of the cell you want to place your charater\n")
 
+def decompile_list():
+    for c in range(size):
+        clist.append(glist[0][c])
+    for c in range(size):
+        clist.append(glist[1][c])
+    for c in range(size):
+        clist.append(glist[2][c])
 
+def compile_list():
+    glist[0][0] = clist [0]
+    glist[0][1] = clist [1]
+    glist[0][2] = clist [2]
+    glist[1][0] = clist [3]
+    glist[1][1] = clist [4]
+    glist[1][2] = clist [5]
+    glist[2][0] = clist [6]
+    glist[2][1] = clist [7]
+    glist[2][2] = clist [8]
+
+def random_choice():
+    cchallenge = True
+    while cchallenge:
+        challenge = random.randint(0,8)
+        if clist[challenge] == "   ":
+            clist[challenge] = " X "
+            cchallenge = False
+        else:
+            cchallenge = True
+
+decompile_list()
+random_choice()
+compile_list()
 print_board()
 
-print_help()
+
